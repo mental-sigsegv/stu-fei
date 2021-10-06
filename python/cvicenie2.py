f107238d705d4f54a5e13f35129d255607b392a0
Klacik <l3mon.off@gmail.com>
2021-10-06 09:45:41 +0200

    update

--- python/cvicenie2.py
+++ python/cvicenie2.py
@@ -1,23 +1,42 @@
+# Uloha 11, 12
+def geo_rad(r, n, c=0, l=[]):
+    for i in range(n):
+        c += r**i
+        l.append(c)
+    return l
+# print(geo_rad(2, 10))
+# print(geo_rad(0.25, 10))
+
+# Uloha 13
+def geo_rad_bu(r, n, c=0, l=[1]):
+    for i in range(n-1):
+        if i == 0:
+            l.append(r+1)
+        else:
+            c = l[-1]+(l[-1]-l[-2])*r
+            l.append(c)
+    return l
+# print(geo_rad_bu(2,10))
+# print(geo_rad_bu(0.25, 10))
+
 # Uloha 15
-def grid(n, k=4):
+def grid(n, k=2):
     for _ in range(n):
         print(('+ ' + '- '*k)*n + '+')
         for _ in range(k):
             print(('| ' + '  '*k)*n + '|')
     print(('+ ' + '- '*k)*n + '+')
-
-grid(3)
+grid(25)
 
 # Uloha 15.1
-# def grid_extended(x, y, k=6):
-#     if x < 1 or y < 1:
-#         print('Invalid num')
-#         return None
-#
-#     for _ in range(y):
-#         print(('+ ' + '- '*k)*x + '+')
-#         for _ in range(k):
-#             print(('| ' + '  '*k)*x + '|')
-#     print(('+ ' + '- '*k)*x + '+')
-#
-# grid_extended(4,2)
+def grid_extended(x, y, k=4):
+    if x < 1 or y < 1:
+        print('Invalid num')
+        return None
+
+    for _ in range(y):
+        print(('+ ' + '- '*k)*x + '+')
+        for _ in range(k):
+            print(('| ' + '  '*k)*x + '|')
+    print(('+ ' + '- '*k)*x + '+')
+# grid_extended(12,4)
