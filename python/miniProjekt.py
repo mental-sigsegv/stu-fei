e08169da7dbb19abb73f02f2273c94f2f4b121f6
Klacik <l3mon.off@gmail.com>
2021-10-13 15:38:28 +0200

    update

--- python/miniProjekt.py
+++ python/miniProjekt.py
@@ -1,49 +1,48 @@
+import turtle
 from turtle import *
 
 pensize(2)
-speed(0)
-# najlepsie urobit 3 funkcie - lupen, stonka, listy => zabalit to do fun kvet() s parametrami
-# odporucane velkosti
-# w = <1, 10>
-# l = <10, 20>
-# k = <5, 35>
+delay(0)
 k = 10 # koeficient kreslenia polkruhu
-def draw_polkruh(w, l):
+
+
+def draw_polkruh(w, l, tur):
     for _ in range(k):
-        fd(l)
-        left(w)
+        tur.fd(l)
+        tur.left(w)
 # draw_polkruh(8, 12)
 
-def draw_list(w, l): # w-width listu, l-length listu
+def draw_list(w, l, tur): # w-width listu, l-length listu
     for _ in range(2):
-        draw_polkruh(w, l)
-        left(180-k*w)
+        draw_polkruh(w, l, tur)
+        tur.left(180-k*w)
 # draw_list(10, 20)
 
-def draw_top(n, w, l): #n-pocet listov; w-width listu, l-length listu
+def draw_top(n, w, l, tur): #n-pocet listov; w-width listu, l-length listu
     for _ in range(n):
-        draw_list(w, l)
-        left(360//n)
+        draw_list(w, l, tur)
+        tur.left(360//n)
 # draw_top(6, 10, 20)
 
-def draw_stopka(w, l):
-    left(180+90-(k-1)*w/2) #pouzil som vypocet uhla v pravidelnych n-uholnikov, konretne vonkajsi uhol
-    draw_polkruh(w, l)
-    setheading(0)
+def draw_stopka(w, l, tur):
+    tur.left(180+90-(k-1)*w/2) #pouzil som vypocet uhla v pravidelnych n-uholnikov, konretne vonkajsi uhol
+    draw_polkruh(w, l, tur)
+    tur.setheading(0)
 # draw_stopka(12, 30)
 
-def draw_bottom(angle, w, l):
-    left(angle-(k-1)*w/2)
-    draw_list(w, l)
-    left(180-2*angle)
-    draw_list(w, l)
+def draw_bottom(angle, w, l, tur):
+    tur.left(angle-(k-1)*w/2)
+    draw_list(w, l, tur)
+    tur.left(180-2*angle)
+    draw_list(w, l, tur)
 # draw_bottom(55, 6, 15)
 
-def kvet(T, S, B):
-    draw_top(*T)
-    draw_stopka(*S)
-    draw_bottom(*B)
+def kvet(T, S, B, tur):
+    draw_top(*T, tur)
+    draw_stopka(*S, tur)
+    draw_bottom(*B, tur)
 
+delay(0)
 def kvety():
         penup(); back(400); left(90); fd(150); pendown(); right(90)
         kvet([7, 6, 14], [8, 40], [30, 4, 10]) # Prvy kvet
@@ -56,6 +55,27 @@ def kvety():
         kvet([10, 2, 12], [1, 30], [45, 7, 20]) # Treti kvet
 # kvety()
 
-kvet([0, 0, 0], [0, 0], [0, 0, 0])
+penup(); fd(1000)
+k1 = turtle.Turtle()
+k1.penup(); k1.back(400); k1.left(90); k1.fd(150); k1.pendown(); k1.right(90)
+k2 = turtle.Turtle()
+k2.penup(); k2.home(); k2.left(90); k2.fd(150); k2.pendown(); k2.right(90)
+k3 = turtle.Turtle()
+k3.penup(); k3.home(); k3.fd(400); k3.left(90); k3.fd(150); k3.pendown(); k3.right(90)
 
+
+import threading
+
+p1 = threading.Thread(target=kvet, args=[[7, 6, 14], [8, 40], [30, 4, 10], k1], daemon=True)
+# p1.daemon = True
+p1.start()
+p2 = threading.Thread(target=kvet, args=([10, 8, 16], [4, 55], [70, 1, 22], k2), daemon=True)
+# p2.daemon = True
+p2.start()
+p3 = threading.Thread(target=kvet, args=([10, 2, 12], [1, 30], [45, 7, 20], k3), daemon=True)
+# p3.daemon = True
+p3.start()
+
+#
+mainloop()
 exitonclick()
--- python/test2.py
+++ python/test2.py
@@ -1,1 +1,16 @@
-import tkinter
+import valve.source
+import valve.source.a2s
+
+# 95.211.1.210:27125 208.64.200.65:27015, 208.64.200.39:27011
+
+host = '95.211.1.210'
+port = 27125
+
+
+server_address = (host, port)
+with valve.source.a2s.ServerQuerier(server_address) as server:
+    info = server.info().get('server_name')
+    players = server.players().get('players')
+    print(dict(list(players)[0]))
+
+import valve.source.a
