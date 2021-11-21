08cfba1f48a8837a67dc1ddc65a36cf1cac09a18
Klacik <l3mon.off@gmail.com>
2021-11-21 01:13:34 +0100

    update

--- clovece/test_with_classes5.py
+++ clovece/test_with_classes5.py
@@ -3,7 +3,7 @@ from random import randint
 class Board():
     def __init__(self):
         self.n = size_of_board
-        self.empty = '()'
+        self.empty = '--'
         self.array = {i:f'{self.empty}' for i in range((self.n-1)*4)}
         self.matrix = []
         self.gen_board()
@@ -19,6 +19,11 @@ class Board():
             self.matrix[s+d][s] = "DD" #self.hracAdomcek[d-1]    #domceky na hore od stredu
             self.matrix[s-d][s] = "DD" #self.hracBdomcek[d-1]    #domceky na dole od stredu
         self.matrix[s][s] = "XX"
+
+        self.matrix[1][s+2] = '↓ '
+        self.matrix[-1][s-2] = ' ↑'
+        self.matrix[s-2][1] = '-→'
+        self.matrix[s+2][-1] = '←-'
 
     def update_board(self):
         s, d = MATRIX_STRED, DLZKA_DOMCEKA
@@ -54,12 +59,11 @@ class Board():
         matrix[s][s+1:s+1+d] = ["DD" for _ in range(DLZKA_DOMCEKA)] if player_B.home == None else player_B.home[::-1]
         matrix[s][s-d:s] = ["DD" for _ in range(DLZKA_DOMCEKA)] if player_D.home == None else player_D.home
 
-
     def print_board(self):
         self.update_board()
         for row in self.matrix:
             print(' '.join(row))
-        print('-'*45)
+        print(P+'-'*45+W)
 
 
 class Player():
@@ -72,48 +76,64 @@ class Player():
     def define_starting_point(self):
         if self.name == 'A':
             self.starting_point = 0
-        if self.name == 'B':
-            self.starting_point = 0
-        if self.name == 'C':
-            self.starting_point = 0
+        elif self.name == 'B':
+            self.starting_point = size_of_board-1
+        elif self.name == 'C':
+            self.starting_point = (size_of_board-1)*2
         else:
-            self.starting_point = 0
+            self.starting_point = (size_of_board-1)*2+size_of_board-1
 
     def move(self):
-        print(f'Na rade je hrac {self.name}')
+        hod_kockou = randint(1,6)
+        # moveable = list(filter(lambda x: x in list(board.array.values()) and list(board.array.values()).index(x)+hod_kockou <= len(board.array), self.figurky))
+        moveable = list(filter(lambda x: x in list(board.array.values()) and len(board.array)+self.starting_point > list(board.array.values()).index(x)+hod_kockou and not ((list(board.array.values()).index(x) < self.starting_point) and (list(board.array.values()).index(x)+hod_kockou >= self.starting_point)), self.figurky))
+        print(f'Na rade je hrac {G+self.name+W}')
+
         if self.name not in ''.join(board.array.values()):
             tri_hody = randint(1, 6), randint(1, 6), randint(1, 6), 6 # TODO: remove this 6
-            print("tri_hody: ", tri_hody)
+            print(f"Tri hody hraca {G+self.name+W}:", ', '.join(map(str, tri_hody)))
             if 6 in tri_hody:
                 board.array.update({self.starting_point:f'{self.name}1'})
                 board.print_board()
                 self.move()
 
-        hod_kockou = randint(1,6)
-        # moveable = dict(filter(lambda x: isinstance(x[1], int) and x[1]+hod_kockou<=len(sachovnica.array)+DLZKA_DOMCEKA, hracA.panacikovia.items()))
-        moveable = list(filter(lambda x: x in list(board.array.values()) and list(board.array.values()).index(x)+hod_kockou <= len(board.array)+DLZKA_DOMCEKA, self.figurky))
-        if hod_kockou == 6 and f'{self.name}' not in board.array[self.starting_point]:
-            print(f"Hod kouckou hraca {self.name}:", hod_kockou) # TODO: while loop ak by input bol nespravny
-            figurka = input(f"{sorted(self.figurky)}"+'\n').upper()
-            board.array.update({self.starting_point:figurka})
-            board.print_board()
+        elif hod_kockou == 6 and f'{self.name}' not in board.array[self.starting_point]:
+            print(f"1Hod kockou hraca {G+self.name+W}:", hod_kockou)
+            figurka = input(f"{sorted(moveable + list(filter(lambda x: x not in list(board.array.values()), self.figurky)))}"+'\n').upper() #TODO: while loop ak by input bol nespravny
+            if figurka not in list(board.array.values()):
+                board.array.update({self.starting_point:figurka})
+                board.print_board()
+            else:
+                position = list(board.array.values()).index(figurka)
+                board.array.update({position:board.empty})
+                position = abs((position+hod_kockou) - ((position+hod_kockou)//len(board.array))*len(board.array))
+                board.array.update({position:figurka})
+                board.print_board()
             self.move()
 
-        elif len(moveable) == 1 and hod_kockou != 6:
+
+        elif len(moveable) == 1:
+            print(f"Hod kockou hraca {G+self.name+W}:", hod_kockou)
             figurka = moveable[0]
+            print('moveabel ', moveable[0])
             position = list(board.array.values()).index(figurka)
-            print(f"Hod kouckou hraca {self.name}:", hod_kockou)
             board.array.update({position:board.empty})
-            board.array.update({position+hod_kockou:figurka})
+            position = abs((position+hod_kockou) - ((position+hod_kockou)//len(board.array))*len(board.array))
+            board.array.update({position:figurka})
             board.print_board()
+            if hod_kockou == 6:
+                self.move()
 
         elif len(moveable) > 1:
-            print(f"Hod kouckou hraca {self.name}:", hod_kockou)
+            print(f"Hod kockou hraca {G+self.name+W}:", hod_kockou)
             figurka = input(f'{moveable}'+'\n').upper() #TODO: while loop ak by input bol nespravny
             position = list(board.array.values()).index(figurka)
             board.array.update({position:board.empty})
-            board.array.update({position+hod_kockou:figurka})
+            position = abs((position+hod_kockou) - ((position+hod_kockou)//len(board.array))*len(board.array))
+            board.array.update({position:figurka})
             board.print_board()
+        else:
+            print(f"Hod kockou hraca {G+self.name+W}:", hod_kockou)
 
 def pravidla_hry():
     print('-'*15+' PRAVIDLA HRY '+'-'*16)
@@ -127,6 +147,15 @@ def pravidla_hry():
     for i, text in enumerate(rules):
         print(f"{i+1}. pravidlo - {text}")
 
+
+# console colors
+W = '\033[0m'  # white (normal)
+R = '\033[31m' # red
+G = '\033[32m' # green
+O = '\033[33m' # orange
+B = '\033[34m' # blue
+P = '\033[35m' # purple
+
 if __name__ == '__main__':
 
 
@@ -136,19 +165,19 @@ if __name__ == '__main__':
     # print()
 
     # urcit pocet hracov
-    # print('Zvolte si pocet hracov od 1 do 4')
-    # while True:
-    #     try:
-    #         num_of_players = input()
-    #         if num_of_players == '':
-    #             raise ValueError('ERROR: Nebolo zadane ziadne cislo')
-    #         num_of_players = int(num_of_players)
-    #         if not 1 <= num_of_players <= 4:
-    #             raise ValueError('ERROR: Napis cele cislo (1/2/3/4)')
-    #         break
-    #     except ValueError as error:
-    #         print(repr(error.args[0]))
-    num_of_players = 1
+    print('Zvolte si pocet hracov od 1 do 4')
+    while True:
+        try:
+            num_of_players = input()
+            if num_of_players == '' or not num_of_players.isnumeric():
+                raise ValueError('ERROR: Nebolo zadane cislo od 1 do 4')
+            num_of_players = int(num_of_players)
+            if not 1 <= num_of_players <= 4:
+                raise ValueError('ERROR: Napis cele cislo (1/2/3/4)')
+            break
+        except ValueError as error:
+            print(R+repr(error.args[0])+W)
+    num_of_players = 4
 
 
     # print(f'Hraci: {", ".join(player.name for player in all_players)}')
@@ -198,8 +227,12 @@ if __name__ == '__main__':
 
     board.print_board()
 
-    for i in range(15):
+    for i in range(10):
         player_A.move()
+        player_B.move()
+        player_C.move()
+        player_D.move()
+
 
 
 
