40d57bae63af248152ddd2866da2d9b83d9c3105
Klacik <l3mon.off@gmail.com>
2022-02-01 14:23:30 +0100

    added gitignore and exercise2

---
+++ c/kniha/cvicenie-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    // Zadanie 1
+    // int i = 5, j = 2;
+    // float f = i / (float)j;
+    // printf("%f", f);
+
+    // Zadanie 2
+    // int a, b;
+    // scanf("%d", &a);
+    // scanf("%d", &b);
+    // (a == b) ? printf("Numbers are equal %d == %d", a, b) : (a > b) ? printf("%d is biger\n%d is smaller", a, b) : printf("%d is biger\n%d is smaller", b, a);
+    // return 0;
+
+    // Zadanie 3
+    
+}
