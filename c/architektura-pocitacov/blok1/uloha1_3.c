01c944cb781d9c1c37d82d80ec321c7d01e26da0
Klacik <l3mon.off@gmail.com>
2022-02-20 14:10:31 +0100

    fixed 3

--- c/architektura-pocitacov/blok1/uloha1_3.c
+++ c/architektura-pocitacov/blok1/uloha1_3.c
@@ -5,15 +5,19 @@ void fibonaci(int k) {
     arr[0] = 1;
     arr[1] = 1;
 
-    for (int i = 2; i < k; i++) {
-        arr[i] = arr[i-1] + arr[i-2];
-    }
+    int i = 1; 
 
-    for (int i = 0; i < k; i++) {
+    printf("%d %d ", arr[0], arr[1]);
+    while (1) {
+        i++;
+        arr[i]=arr[i-1]+arr[i-2];
+        if (arr[i] > k) {
+            break;
+        }
         printf("%d ", arr[i]);
     }
 }
 
 int main() {
-    fibonaci(1);
+    fibonaci(145);
 }
