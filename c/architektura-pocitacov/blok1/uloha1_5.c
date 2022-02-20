c7ac982aa087f919f5711e088f15280b951bc1ca
Klacik <l3mon.off@gmail.com>
2022-02-21 00:27:54 +0100

    commit

--- c/architektura-pocitacov/blok1/uloha1_5.c
+++ c/architektura-pocitacov/blok1/uloha1_5.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
+
+double degToRad(int deg) {
+    return (deg%180)/(double)180;
+}
+
 int fact(int k) {
     int tmp = 1;
     for (int i = 1; i <= k; i++) {
@@ -11,14 +16,22 @@ int fact(int k) {
 
 int main() {
     // cos
-    double arr[20];
-    double x = 0.1;
+    long int k = 12;
+    int degree = 100;
+    double arr[k];
+    double x = degToRad(degree);
+    
+    printf("Deg:      %d\n", degree);
     arr[0] = 1;
 
-    for (int n = 1; n < 20; n++) {
+    for (int n = 1; n < k; n++) {
         arr[n] = arr[n-1] + ((pow(-1, n))*(pow(x, 2*n)))/fact(2*n);
     }
 
-    printf("%lf", arr[1]);
+    printf("Cos:      %.20lf\n", cos(x));
+    printf("Mine cos: %.20lf\n", arr[k-1]);
+    // printf("%d", cos(x) == arr[k-1]);
+
+    return 0;
 }
 
