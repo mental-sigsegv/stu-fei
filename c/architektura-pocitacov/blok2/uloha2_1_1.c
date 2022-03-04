2f0793640e4f2f7f4ce1937779c0c9af9431065e
Klacik <l3mon.off@gmail.com>
2022-03-05 00:40:07 +0100

    commit

--- c/architektura-pocitacov/blok2/uloha2_1_1.c
+++ c/architektura-pocitacov/blok2/uloha2_1_1.c
@@ -5,11 +5,14 @@ static int result, x, y;
 void addNumbers() {
     x = 34; y = 72;
     asm(".intel_syntax noprefix \n"   // Prepneme z AT&T syntaxe na na Intel 
-       "movabs eax, x\n"
-       "mov ebx, eax\n"
-       "movabs eax, y\n"
-       "add eax, ebx\n"
-       "movabs result, eax\n"
+    //    "movabs eax, x\n"
+    //    "mov ebx, eax\n"
+    //    "movabs eax, y\n"
+    //    "add eax, ebx\n"
+    //    "movabs result, eax\n"
+
+        // "movabs eax, x\n"
+        "movabs edx, x\n"
 
        ".att_syntax            \n"); // Dame vsetko do povodneho stavu
 
--- c/architektura-pocitacov/test1.c
+++ c/architektura-pocitacov/test1.c
@@ -4,16 +4,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-static int iVysledok, iCislo;
-
 int main() {
-    asm(".intel_syntax noprefix \n"   // Prepneme z AT&T syntaxe na na Intel 
-   
-       "movabs eax, iCislo        \n"   // iCislo -> EAX
-       "inc eax                \n"   // EAX ++ 
-       "movabs iVysledok,eax     \n"   // EAX  -> iVysledok  
-
-       ".att_syntax            \n");
-    
-    printf("%d\n%d", iVysledok, iCislo);
+   int c;
+   c += 10;
+   printf("%d", c);
 }
