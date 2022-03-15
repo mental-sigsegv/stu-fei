fd33ddaad48362dd2cf8ddc238d5717dcfbd32ea
Klacik <l3mon.off@gmail.com>
2022-03-15 22:59:06 +0100

    commit

--- c/prog2/test.c
+++ c/prog2/test.c
@@ -2,31 +2,9 @@
 #include <stdlib.h>
 
 
-void edit_arr(int *arr_en, int *arr_ex) {
-    int tmp = arr_en[1];
-    arr_en[1] = arr_en[2];
-    arr_en[2] = tmp;
-
-    tmp = arr_ex[1];
-    arr_ex[1] = arr_ex[2];
-    arr_ex[2] = tmp;
-
-}
-
 int main() {
-    int enter[] = {1, 7, 5};
-    int exit[] = {10, 6, 8};
-
-    for (int i = 0; i < 3; i++) {
-        printf("[%d,%d]\n", enter[i], exit[i]);
-    }
-    printf("\n");
-
-    edit_arr(enter, exit);
-
-    for (int i = 0; i < 3; i++) {
-        printf("[%d,%d]\n", enter[i], exit[i]);
-    }
-
+    int num;
+    scanf("%d", &num);
+    printf("num is %d", num);
     return 0;
 }
