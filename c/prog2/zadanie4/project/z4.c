82970556ceab60917544a39ab3c38cdabc78bbe7
Martin Klacik <klacikmartin@protonmail.com>
2022-04-13 16:04:17 -0400

    done

--- c/prog2/zadanie4/project/z4.c
+++ c/prog2/zadanie4/project/z4.c
@@ -8,7 +8,6 @@
 #include "config.h"
 // #include "functions.c"
 
-
 static int fw_NAME=0, fw_ITEM=0, fw_LAT=0, fw_LON=0, fi_NAME=0, fi_PRICE=0, FORMAT=0; 
 static char *fw_NAMEs, *fw_ITEMs, *fi_NAMEs;
 static char *fw_LONf, *fw_LATf;
@@ -158,49 +157,34 @@ int main(int argc, char *argv[]){
                 FORMAT = 1;
                 break;
             case ':':
-                printf("return 2, missing argument");  // TODO REMOVE
                 return 2;
             default: /* '?' alebo ':' */   
-                // printf("%d ", opt);
-                // for (int l = 1; l <= (int)strlen(optstring); l=l+2) {
-                //     // printf("%d ", optstring[l]);
-                //     if (optstring[l] ==  optopt) {
-                //         printf("req. arg");
-                //         return 2;
-                //     }
-                // }
-                printf("return 1, wrong switch");  // TODO REMOVE
                 return 1;
         }
     }
 
     // Return 3, one of the flags are 0
     if ((fw_LON || fw_LAT) && (fw_LON != fw_LAT)) {
-            printf("return 3, missing -n / -e");  // TODO REMOVE
             return 3;
     } 
     
-    // Return 4  // ? optimize
+    // Return 4
     if ((fw_LON == 1) && (fw_LAT == 1)) {
         if (isNumeric(fw_LONf)) {
             GPSposition.lon = atof(fw_LONf);
         } else {
-            printf("return 4");  // TODO remove
             return 4;
         }
         if ((GPSposition.lon < LON_MIN) || (GPSposition.lon > LON_MAX)) {
-            printf("return 4, lon wrong interval");  // TODO remove
             return 4;
         }
 
         if (isNumeric(fw_LATf)) {
             GPSposition.lat = atof(fw_LATf);
         } else {
-            printf("return 4");  // TODO remove
             return 4;
         }
         if ((GPSposition.lat < LAT_MIN) || (GPSposition.lat > LAT_MAX)) {
-            printf("return 4, lat wrong interval");  // TODO remove
             return 4;
         }
     }
@@ -213,12 +197,6 @@ int main(int argc, char *argv[]){
     } else if ((fw_LON == 1) && (fw_LAT == 1)) {
         fw_byGps(GPSposition);
     }
-
-    // TODO REMOVE
-    // printf("\n");
-    // for (int i = 0; i < DB_NUM; i++) {
-    //     printf("%d ", validWarehouse[i]);
-    // }
 
     // Print
     if (FORMAT == 1) {
