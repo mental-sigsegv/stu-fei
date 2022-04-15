e37092e4aeb1e2cf2a13ff7e25dec38f3aef7d12
Martin Klacik <klacikmartin@protonmail.com>
2022-04-15 15:29:25 -0400

    changed gitignore and debug files

--- c/architektura-pocitacov/blok3/blok3.c
+++ c/architektura-pocitacov/blok3/blok3.c
@@ -4,7 +4,7 @@
 #include<arpa/inet.h>	//inet_addr
 
 const char* SERVER_ADRESS = "147.175.115.34";
-const short unsigned int SERVER_PORT = 80;
+const short unsigned int SERVER_PORT = 777;
 
 int main()
 {
@@ -15,7 +15,7 @@ int main()
 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 	if (socket_desc == -1)
 	{
-		printf("Could not create socket");
+		printf("-> Could not create socket");
 	}
 		
 	server.sin_addr.s_addr = inet_addr(SERVER_ADRESS);
@@ -25,15 +25,25 @@ int main()
 	//Connect to remote server
 	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
 	{
-		puts("Connect error...");
+		puts("-> Connect error...");
 		return 1;
 	}
 	
-	puts("Connected...\n");
+	puts("-> Connected...\n");
 
-    //Send some data
+
+    // Receive a reply from the server
+    char server_reply[4096];
+	if( recv(socket_desc, server_reply , 4096 , 0) < 0)
+	{
+		puts("-> recv failed");
+	}
+	puts("-> Reply received:");
+	puts(server_reply);
+
+	//Send some data
     char *message;
-	message = "GET / HTTP/1.1\r\n\r\n";
+	message = "115330\r\n\r\n\0";
 	if( send(socket_desc , message , strlen(message) , 0) < 0)
 	{
 		puts("Send failed");
@@ -41,14 +51,13 @@ int main()
 	}
 	puts("Data was send...\n");
 
-    //Receive a reply from the server
-    char* server_reply[4096];
-	if( recv(socket_desc, server_reply , 2000 , 0) < 0)
+	// Receive a reply from the server
+	char server_reply2[4096];
+	if( recv(socket_desc, server_reply2 , 4096 , 0) < 0)
 	{
-		puts("recv failed");
+		puts("-> recv failed");
 	}
-	puts("Reply received\n");
-	puts(server_reply);
-
+	puts("-> Reply received:");
+	puts(server_reply2);
 	return 0;
 }
