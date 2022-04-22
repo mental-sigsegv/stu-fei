0ddcfc5ded14c1751c5e743371925946104d18da
Martin Klacik <klacikmartin@protonmail.com>
2022-04-22 04:54:23 -0400

    import default files

---
+++ c/prog2/zadanie5/z5.c
@@ -0,0 +1,8 @@
+#include "config.h"
+// TODO 
+// Vase vlastne pomocne makra, datove typy a funkcie.
+int main(int argc, char *argv[]){
+    // TODO 
+    // Implementacia hlavnej logiky programu.
+    return 0;
+}
