2466b65ff8e0a3a7c119b07689e6dfc143a13d06
Martin Klacik <klacikmartin@protonmail.com>
2022-04-26 12:29:45 -0400

    blok3

---
+++ c/architektura-pocitacov/blok3/ais/hlavickovy_subor.h
@@ -0,0 +1,9 @@
+void sendMessage(char *message);
+void format_print(char* message);
+void recieveMessage();
+int compute_code(char* num);
+char* itoa(int val, int base);
+int is_prime(int num);
+void decipher_by_primes();
+void decipher();
+void decipher_by_caesar(char* cipher, int rotate);
