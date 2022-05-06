9adc067889781593281f2c04a242b7f293e28f2d
Martin Klacik <klacikmartin@protonmail.com>
2022-05-06 08:23:52 -0400

    simple text db

---
+++ c/prog2/test-files/script.py
@@ -0,0 +1,12 @@
+import json
+
+x = set()
+
+with open("c/prog2/test-files/script.txt", "r") as f:
+    f_data = f.read().splitlines()
+    # print(f_data)
+
+for word in f_data:
+    x.add(word.strip())
+
+print(repr(x).replace("'", '"'))
