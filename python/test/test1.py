75ebaa483bbc922cb6c0e73b66fcc69fafd3a74a
Klacik <l3mon.off@gmail.com>
2022-05-11 11:43:56 +0200

    python cv2 test

--- python/test/test1.py
+++ python/test/test1.py
@@ -1,5 +1,56 @@
-nums = 15
-x = [j for j in range(5) for i in range(5)]
+# Setting
+ios_width = 1200
+ios_height = 700
 
-print(x)
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import time
+
+# just so this doesn't go on forever:
+
+def screen_record(): 
+    last_time = time.time()
+    while(True):
+        # 800x600 windowed mode
+        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        print('loop took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+        cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+            
+#screen_record()
+
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import time
+
+
+def process_img(image):
+    original_image = image
+    # convert to gray
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # edge detection
+    processed_img =  cv2.Canny(processed_img, threshold1 = 600, threshold2=600, apertureSize = 3)
+    return processed_img
+
+def main():
+    last_time = time.time()
+    while True:
+        screen =  np.array(ImageGrab.grab(bbox=(0,40,ios_width, ios_height)))
+        #print('Frame took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+        new_screen = process_img(screen)
+        # cv2.imshow('window', new_screen)
+        cv2.imshow('window',cv2.cvtColor(new_screen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+main()
+# from IPython.display import Image
+# Image(filename='edge-detection.png') 
 
