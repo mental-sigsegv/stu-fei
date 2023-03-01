e41d26fdd53c34a31e55ea5c0440938a914cd295
Martin Klacik <klacikmartin@protonmail.com>
2023-03-01 13:52:04 +0100

    uloha 2.2

---
+++ java/uloha_2_2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class uloha_2_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String text= input.nextLine();
+
+    System.out.println(text);
+
+    }
+}
